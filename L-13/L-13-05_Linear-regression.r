0fe1e61fdc5bee503d8b07dbc48c06dc046d232e
urs-dev <urs.klahr@bluewin.ch>
2019-09-25 10:51:29 +0200

    L-13: done (with source)

--- L-13/L-13-05_Linear-regression.r
+++ L-13/L-13-05_Linear-regression.r
@@ -27,25 +27,79 @@ library(readxl)
 
 
 # ---------------------------------------------------------
+#   1   test data: cars
+# ---------------------------------------------------------
+head(cars)
+
+
+# ---------------------------------------------------------
+#   2   first look at the data
+# ---------------------------------------------------------
+ggplot(data = cars,
+       mapping = aes(x = speed, y = dist)) +
+  geom_point() +
+  # geom_smooth(method = "lm", se = TRUE) +
+  labs(title = "The Relationship between Speed and Stopping Distance",
+       x = "Speed",
+       y = "Stopping Distance")
+
+
+
+# ---------------------------------------------------------
+#   3   build 'linear regression model'
 #
-# ---------------------------------------------------------
-
-
-
-# ---------------------------------------------------------
+#       LM: dist = X + Y*speed
 #
+#           - independent = 'speed'
+#           -   dependent = 'dist'
 # ---------------------------------------------------------
+cars_lm <- lm(dist ~ speed, data = cars)
 
+cars_lm
 
-
-
-# ---------------------------------------------------------
+# Result
 #
+#   LM:  dist = -17.579  +  ( 3.932 * speed)
+
+
 # ---------------------------------------------------------
+#   4   visual check the values of LM
+# ---------------------------------------------------------
+cars$predicted <- cars_lm$fitted.values
 
-
+ggplot(data = cars,
+       mapping = aes(x = dist, y = predicted)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +
+  labs(title = "Stopping Distance: Predicted and Observed Values",
+       x = "Actual Stopping Distance",
+       y = "Predicted Stopping Distance")
 
 # ---------------------------------------------------------
+#   5   check the values of LM    --> using 'p-values'
+# ---------------------------------------------------------
+summary(cars_lm)
+
+# result
 #
+#   p-value = 1.49e-12
+#   --> p-values is less than 0.05  --> reasonable good   --> "model is statistically significant"
+
+
+
+
 # ---------------------------------------------------------
+#   6   test new values
+# ---------------------------------------------------------
+cars_new <- tibble(speed = c(30, 42, 37, 48, 60))
+
+predict(object = cars_lm,  newdata = cars_new)
+
+
+
+
+
+
+
+
 
